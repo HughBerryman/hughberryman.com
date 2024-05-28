3ccd4b26a3bd5e16d399b261aa526aaef117ed33
Hugh <hgberryman8@gmail.com>
2024-05-28 15:45:31 +0500

    adding hotjar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nuxt-icon", "@nuxt/image"], // https://nuxt.com/modules/icon
+  modules: ["nuxt-icon", "@nuxt/image", "nuxt-module-hotjar"], // https://nuxt.com/modules/icon
+  // And add your hotjar site id and the script version
+  hotjar: {
+    hotjarId: 5001530,
+    scriptVersion: 6,
+    // optionally you can turn on debug mode for development
+    debug: process.env.NODE_ENV === "development",
+  },
 });
